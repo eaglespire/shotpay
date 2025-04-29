44b7a57ff13616e71f65bd6d5cc2692905ddb76d
eaglespire <eaglespiresolutions@gmail.com>
2025-04-29 05:06:18 +0100

    dhdhdhd

---
+++ app/Http/Controllers/SumsubController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+class SumsubController extends Controller
+{
+    public function createLink(Request $request)
+    {
+
+    }
+}
