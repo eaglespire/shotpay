f32b97c6c02345ac53386e66744824298cd5f5ea
eaglespire <eaglespiresolutions@gmail.com>
2025-04-29 04:52:12 +0100

    dhdhdhd

---
+++ config/settings.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'sumsub_secret' => env('SUMSUB_SECRET',''),
+    'sumsub_base' => env('SUMSUB_BASE',''),
+    'sumsub_token' => env('SUMSUB_TOKEN',''),
+    'sumsub_level' => env('SUMSUB_LEVEL','')
+];
